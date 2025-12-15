4544e7e17ebbf7d7f2af7d3a4ce44ce48c864a33
Aysegul-hub <ayseguldelialioglu20@gmail.com>
2025-12-15 22:43:49 +0300

    commit

--- src/HW1_Task1/Task1Test.java
+++ src/HW1_Task1/Task1Test.java
@@ -3,10 +3,10 @@ public class Task1Test {
         PatientList list = new PatientList();
 
         list.addPatient(new Patient(1 , "Ali Murat Budak" , 5 , 30));
-        list.addPatient(new Patient(1 , "Leyla Parlak" , 8 , 18));
-        list.addPatient(new Patient(1 , "Tahir Aslan" , 3 , 25));
-        list.addPatient(new Patient(1 , "Mehtap Ece Şafak" , 6 , 45));
-        list.addPatient(new Patient(1 , "Hakan Çavuş" , 9 , 60));
+        list.addPatient(new Patient(2 , "Leyla Parlak" , 8 , 18));
+        list.addPatient(new Patient(3 , "Tahir Aslan" , 3 , 25));
+        list.addPatient(new Patient(4 , "Mehtap Ece Şafak" , 6 , 45));
+        list.addPatient(new Patient(5 , "Hakan Çavuş" , 9 , 60));
 
         System.out.println("Patient list after adding patients: ");
         list.printList();
@@ -15,7 +15,7 @@ public class Task1Test {
         System.out.println("\nAfter removing patient using by ID 3 : ");
         list.printList();
 
-        System.out.println("\nSearching for patient with ID 4: ");
+        System.out.println("\nSearching for patient with ID 4: "); //
         System.out.println(list.findPatientList(4)); 
 
     }
